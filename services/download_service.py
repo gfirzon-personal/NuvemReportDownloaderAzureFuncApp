5ea5b22657e4554d2663df3cefefd77580cc51a8
Gary Firzon <gfirzon@yahoo.com>
2024-11-09 11:51:34 -0600

    continue work

--- services/download_service.py
+++ services/download_service.py
@@ -3,7 +3,7 @@
 import ssl
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 # import settings
-from services import azureSettings, settings
+from services import azSettings, settings
 
 class DownloadService:
     def __init__(self):
@@ -15,6 +15,6 @@ class DownloadService:
         context = ssl._create_unverified_context()
         
         # Azure Blob Storage connection string and container name
-        connection_string = azureSettings.azure_storage_connection_string
+        connection_string = azSettings.azure_storage_connection_string
         container_name = settings.azure_storage_container_name
         blob_name = settings.blob_name
