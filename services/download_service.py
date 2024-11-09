ef980af3fd6bd3acbfb8fa4f1fb30d43fd1006d3
Gary Firzon <gfirzon@yahoo.com>
2024-11-09 12:01:15 -0600

    file uploaded to blob

--- services/download_service.py
+++ services/download_service.py
@@ -1,8 +1,7 @@
-#import http.client
+import http.client
 #import os
 import ssl
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-# import settings
 from services import azSettings, settings
 
 class DownloadService:
@@ -18,3 +17,34 @@ class DownloadService:
         connection_string = azSettings.azure_storage_connection_string
         container_name = settings.azure_storage_container_name
         blob_name = settings.blob_name
+
+        conn = http.client.HTTPSConnection("340bopais.hrsa.gov", context=context)
+
+        payload = settings.payload
+        headers = settings.headers
+
+        conn.request("POST", "/reports?AspxAutoDetectCookieSupport=1", payload, headers)
+        res = conn.getresponse()
+
+        # Check if the response is successful and content type is Excel
+        if res.status == 200 and res.getheader('Content-Type').startswith('application/vnd'):
+            print("Starting file download...")
+
+            # Read data from the response
+            data = res.read()
+
+            # Create a BlobServiceClient object
+            blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+            # Get a container client
+            container_client = blob_service_client.get_container_client(container_name)
+
+            # Upload the file to the container
+            blob_client = container_client.get_blob_client(blob_name)
+            blob_client.upload_blob(data, overwrite=True)    
+            print("File uploaded to Azure Blob Container successfully.")
+        else:
+            print(f"Failed to download file: {res.status} {res.reason}")  
+
+        # Close the connection
+        conn.close()                  
